5c72661a8dff8f59251ee66a0ad69a7b1153e425
afeiship <1290657123@qq.com>
2024-10-23 11:13:31 +0800

    feat: add types

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+interface NxStatic {
+  sample: (arr: any, count?: number) => any;
+}
--- src/index.js
+++ src/index.js
@@ -1,30 +1,30 @@
-(function () {
-  var global = global || this || window || Function('return this')();
-  var nx = global.nx || require('@jswork/next');
-  var sample = function (inTarget) {
-    var length = inTarget.length;
-    if (typeof length !== 'number') return;
-    var index = Math.floor(Math.random() * length);
-    return inTarget[index];
-  };
+import nx from '@jswork/next';
 
-  nx.sample = function (inTarget, inCount) {
-    if (inTarget.length <= inCount) return inTarget;
-    if (inCount === void(0)) {
-      return sample(inTarget);
-    } else {
-      var result = [];
-      while (result.length !== inCount) {
-        var value = sample(inTarget);
-        if (result.indexOf(value) === -1) {
-          result.push(value);
-        }
+const sample = function (inTarget) {
+  var length = inTarget.length;
+  if (typeof length !== 'number') return;
+  var index = Math.floor(Math.random() * length);
+  return inTarget[index];
+};
+
+nx.sample = function (inTarget, inCount) {
+  if (inTarget.length <= inCount) return inTarget;
+  if (inCount === void 0) {
+    return sample(inTarget);
+  } else {
+    var result = [];
+    while (result.length !== inCount) {
+      var value = sample(inTarget);
+      if (result.indexOf(value) === -1) {
+        result.push(value);
       }
-      return result;
     }
-  };
-
-  if (typeof module !== 'undefined' && module.exports) {
-    module.exports = nx.sample;
+    return result;
   }
-})();
+};
+
+if (typeof module !== 'undefined' && module.exports && typeof wx === 'undefined') {
+  module.exports = nx.sample;
+}
+
+export default nx.sample;
